14aba17542d7cb7798f5dca1d80978d3e5088398
Kyle Hill <kylehill@gmail.com>
2015-06-04 11:54:21 -0400

    Finishes in-class version of this game

--- public/index.js
+++ public/index.js
@@ -1,33 +1,48 @@
 var timeouts = {}
+var level = 1
 
 var addAHippo = function() {
   var $newHippo = $("<div class='hippo'></div>")
   $(".game-container").append($newHippo)
 }
+var addAnOyster = function() {
+  var $newOyster = $("<div class='oyster'></div>")
+  $newOyster.on("mouseover", mouseoverOyster)
+  $(".game-container").append($newOyster)
+}
 
-var initializeGame = function() {
-  for (var i = 0; i < 6; i++) {
+var mouseoverOyster = function() {
+  $(".hippo:first").remove()
+  
+  if ($(".hippo").length === 0) {
+    level = level + 1
+    initializeLevel(level)
+  }
+
+  var containerHeight = $(".game-container").height()
+  var containerWidth = $(".game-container").width()
+
+  var leftProperty = (Math.random() * (containerWidth - 70))
+  var topProperty = (Math.random() * (containerHeight- 70))
+
+  $(".oyster").css({
+    left: leftProperty + "px",
+    top: topProperty + "px"
+  })
+}
+
+var initializeLevel = function(level) {
+  for (var i = 0; i < (level + 5); i++) {
     addAHippo()
   }
 
-  var startTime = new Date().valueOf()
+  var r = Math.round( Math.random() * 255 )
+  var g = Math.round( Math.random() * 255 )
+  var b = Math.round( Math.random() * 255 )
 
-  $(".end-message").removeClass("active")
+  $(".game-container").css("background-color", "rgb(" + r + "," + g + "," + b + ")")
 
-  timeouts.checkForCollision = setInterval(function(){
-    $(".hippo:hover").each(function(){
-      $(".hippo").remove()
-      $(".end-message").addClass("active")
-
-      clearInterval(timeouts.checkForCollision)
-      clearInterval(timeouts.hippoMove)
-      clearInterval(timeouts.hippoMultiply)
-
-      var endTime = new Date().valueOf()
-
-      $("#scoreText").text((endTime - startTime) / 1000)
-    })
-  }, 100)
+  clearInterval(timeouts.hippoMove)
 
   timeouts.hippoMove = setInterval(function(){
     var containerHeight = $(".game-container").height()
@@ -36,8 +51,10 @@ var initializeGame = function() {
     $(".hippo").each(function(){
       var $hippo = $(this)
 
-      var deltaTop = (Math.random() * 200) - 100
-      var deltaLeft = (Math.random() * 200) - 100
+      var moveAmount = 75 + (level * 25)
+
+      var deltaTop = (Math.random() * (2 * moveAmount)) - moveAmount
+      var deltaLeft = (Math.random() * (2 * moveAmount)) - moveAmount
 
       var currentTop = parseFloat($hippo.css("top"))
       var currentLeft = parseFloat($hippo.css("left"))
@@ -66,6 +83,30 @@ var initializeGame = function() {
     })
     
   }, 500)
+}
+
+var initializeGame = function() {
+  level = 1
+  initializeLevel(level)
+
+  var startTime = new Date().valueOf()
+
+  $(".end-message").removeClass("active")
+  addAnOyster()
+
+  timeouts.checkForCollision = setInterval(function(){
+    $(".hippo:hover").each(function(){
+      $(".hippo").remove()
+      $(".oyster").remove()
+      $(".end-message").addClass("active")
+
+      clearInterval(timeouts.checkForCollision)
+      clearInterval(timeouts.hippoMove)
+      clearInterval(timeouts.hippoMultiply)
+
+      $("#scoreText").text(level)
+    })
+  }, 100)
 
   timeouts.hippoMultiply = setInterval(addAHippo, 10000)
 }
